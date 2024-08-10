ac2d2fc733ad1cdfe5c36b4e071bf82b316695b4
Moyo Oyeniyi <moyosoreoluwaoyeniyi@gmail.com>
2024-08-10 15:54:38 +0100

    Added Axios and API to JavaScript file

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+function displayWeather(response){
+console.log(response.data)
+}
 function searchCity(event){
   event.preventDefault()
 
@@ -5,10 +8,15 @@ function searchCity(event){
 
   let h1 = document.querySelector("h1")
   h1.innerHTML = searchText.value
+  let city = searchText.value
+  let key = "d0f486766tb3do97a754aa844599ab3a"
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${key}`
 
+axios.get(apiUrl).then(displayWeather)
   
 }
 
 let searchButton = document.querySelector("#search-button")
 
 searchButton.addEventListener("click", searchCity)
+
